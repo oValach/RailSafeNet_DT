e5a1d27dbd2d9440ad118a60e7e5a08e35826015
Ondřej Valach <65716432+oValach@users.noreply.github.com>
2024-05-19 21:27:34 +0200

    Delete image_resize.py

--- image_resize.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import os
-
-PATH_jpgs = 'RailNet_DT/rs19_val/jpgs/vozovna'
-for filename_img in os.listdir(PATH_jpgs):
-
-    image = cv2.imread(os.path.join(PATH_jpgs, filename_img))
-    resized_image = cv2.resize(image, (1920, 1080))
-    cv2.imwrite('RailNet_DT/rs19_val/jpgs/vozovna/{}_res.png'.format(filename_img), resized_image)
